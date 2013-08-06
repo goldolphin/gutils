c26abd616bb8f166f3dfceba12aea9cba56fcd3c
goldolphin <goldolphin@gmail.com>
2013-08-05 23:56:15 -0700

    MOD: compare_extraction.ml

--- ocaml/utils.ml
+++ ocaml/utils.ml
@@ -2,7 +2,9 @@
 let (|>) f g =
   g f
 
-let in_stream in_channel =
+let identity a = a
+
+let channel_stream in_channel =
   Stream.from (fun count ->
     try
       Some (input_line in_channel)
@@ -10,7 +12,20 @@ let in_stream in_channel =
         None
   )
   
-let file_stream filename = in_stream (open_in filename)
+let file_stream filename = channel_stream (open_in filename)
 
 let printfn format =
   Printf.kfprintf (fun ch -> output_char ch '\n') stdout format
+
+let rec last_of_list list =
+  match list with
+  | head::[] -> head
+  | head::tail -> last_of_list tail
+  | [] -> invalid_arg "Empty list!"
+
+let list_to_string converter list =
+  "[" ^ (List.map converter list |> String.concat "; ") ^ "]"
+
+module StrMap = Map.Make(String)
+
+module IntMap = Map.Make(Int32)
